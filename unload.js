7f766d6ea3178acbc2c6095e79403b0397b359d0
Bogdan Filipchuk <bogdanfilipchuk@gmail.com>
2019-01-25 09:51:33 -0800

    Add disable and hide
    - Disable menu on unloaded tabs.
    - Hide for 'about:*' tabs.
    - Added 'tabs' permission.

--- unload.js
+++ unload.js
@@ -1,19 +1,73 @@
+var MENU_ID = 'unload-tab';
+var APP_NAME = 'Unload Tab';
 
-// Add Unload Tab option to context menu
+function unloadTab(item, tab) {
+	// Can't unload active tab.
+	if(tab.active) {
+		return browser.windows.get(tab.windowId, {populate: true}).then(window => {
+			console.log(APP_NAME, 'Tab is Active', window);
+			function adjust(index) {
+				return index < tab.index ? index + window.tabs.length : index;
+			}
 
+			var tabs = window.tabs;
+			var dis = tabs
+				// Filter discarded tabs and current tab.
+				.filter(cur => !cur.discarded && cur.id != tab.id);
+			var active = dis
+				// Pick next active window from ring relative to tab
+				.reduce((acc, cur) => {
+					if(acc === undefined) return cur;
+					return adjust(cur.index) < adjust(acc.index) ? cur : acc;
+				});
+
+			return active
+				// Set the next tab to active
+				? browser.tabs.update(active.id, {active: true})
+				// Create a new tab if all are disabled. Set it active.
+				: browser.tabs.create({active: true, index: tab.index + 1});
+				//: browser.tabs.create({active: true, index: window.tabs.length});
+		}, console.error).then(() => browser.tabs.discard(tab.id));
+	} else {
+		return browser.tabs.discard(tab.id);
+	}
+}
+
+// Add new entry to context menu for browser tabs.
 browser.menus.create({
-	id: 'unload-tab',
+	id: MENU_ID,
 	type: 'normal',
 	title: 'Unload Tab',
+	enabled: true,
 	contexts: ['tab'],
-}, function() {
+	icons: null,
+	// documentUrlPatterns: [],
+	// targetUrlPatterns: [],
+	onclick: unloadTab,
+}, () => {
 	if (browser.runtime.lastError) {
-		console.log('Unload Tab: Error creating menu item. ' + browser.runtime.lastError);
+		console.error('Unload Tab: Error creating menu item. ' + browser.runtime.lastError);
 	}
 });
 
+// Show/Hide entry based on unloaded state of tab
+browser.menus.onShown.addListener(function(item, tab) {
+
+	// Update menu with status of tab
+	browser.menus.update(MENU_ID, {
+		enabled: !tab.discarded,
+		visible: !(tab.url && tab.url.startsWith('about:')),
+	});
+
+	// Refresh menu to show changes
+	browser.menus.refresh();
+});
+
+
+
 browser.menus.onClicked.addListener(function(info, tab) {
-	if (info.menuItemId == "unload-tab") {
+	return;
+	if (info.menuItemId == MENU_ID) {
 		browser.tabs.get(tab.id)
 		.then(function(tab_info) {
 			if (tab_info.discarded) {
